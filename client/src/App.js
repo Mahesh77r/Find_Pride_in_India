80a7f0f9ae87579663f405dc497e9e88594f7930
Ashwiniukhalkar <ashwiniukhalkar1@gmail.com>
2023-09-21 22:18:43 +0530

    table

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './pages/loginPage';
+import Table from './components/Table/Table';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/table" element={<Table />} />
     </Routes>
     </BrowserRouter>
     </>
