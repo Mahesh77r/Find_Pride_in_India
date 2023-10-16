b4e09fcdb3ddef283eed5d3685471ad798feeba7
Mahesh Rohane <shravanrohane19@gmail.com>
2023-10-16 09:52:26 +0530

    edit forms build

--- client/src/components/Cards/Cards.js
+++ client/src/components/Cards/Cards.js
@@ -1,15 +1,16 @@
-import {React,useState} from "react";
+import { React, useState } from "react";
 import "./Cards.css";
 import hosp from '../../images/hostp.jpeg'
-import {Link} from 'react-router-dom';
-import { DeleteModal } from "../Modal/Modal";
+import { Link } from 'react-router-dom';
+import { DeleteModal, EditModal } from "../Modal/Modal";
+import { FormTouristGuide,FormEvents ,FormFacility,FormProduct} from "../Forms/ManagementsForms";
 
 
 export const GuideCards = ({ guidename, img_url, fees, contact_number }) => {
   return (
     <div className="rounded bgf overflow-hidden shadow-lg transform transition-transform hover:scale-105">
       {/* Buttons */}
-      <EditDeletebuttons/>
+      <UpdateDeletebuttons Updateform={<FormTouristGuide />} update_delete_title={"Guide"} />
 
       {/*  */}
       <div className="row grid">
@@ -36,7 +37,7 @@ export const EventCard = ({ event_name, image_url, descp }) => {
     <>
       <div className="rounded bgf item-center overflow-hidden shadow-lg transform transition-transform hover:scale-105">
         {/* Buttons */}
-        <EditDeletebuttons/>
+        <UpdateDeletebuttons Updateform={<FormEvents/>} update_delete_title={"Event"} />
 
         {/* Image */}
         <div className="row grid">
@@ -56,7 +57,7 @@ export const EventCard = ({ event_name, image_url, descp }) => {
 };
 
 export const AddCards = ({ DataName }) => {
-  
+
   return (
     <>
       <div className="rounded bgf item-center overflow-hidden shadow-lg transform transition-transform hover:scale-105">
@@ -89,7 +90,7 @@ export const ProductCard = ({ img_url, prod_name, prod_price, prod_quant, prod_d
     <>
       <div className="rounded bgf item-center overflow-hidden shadow-lg transition-transform hover:scale-105">
         {/* Buttons */}
-        <EditDeletebuttons/>
+        <UpdateDeletebuttons Updateform={<FormProduct/>} update_delete_title={"Product"} />
 
         {/* Image */}
         <div className="row rounded-lg">
@@ -115,12 +116,12 @@ export const ProductCard = ({ img_url, prod_name, prod_price, prod_quant, prod_d
   );
 };
 
-export const FacilityCard = ({fac_name,fac_img_url,fac_cont,fac_loact}) => {
+export const FacilityCard = ({ fac_name, fac_img_url, fac_cont, fac_loact }) => {
   return (
     <>
       <div className="rounded bgf  border border-solid border-cyan-800 item-center overflow-hidden shadow-lg transition-transform hover:scale-105">
         {/* Buttons */}
-        <EditDeletebuttons/>
+        <UpdateDeletebuttons Updateform={<FormFacility/>} update_delete_title={"Facility"} />
         {/* Image */}
         <div className="row ">
           <img className="w-100 h-30 py-2 px-5" alt="Events " src={hosp} />
@@ -142,14 +143,14 @@ export const FacilityCard = ({fac_name,fac_img_url,fac_cont,fac_loact}) => {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
                 </svg>
-                <span className="">Visit </span>
+                <span className="ms-1">Visit</span>
               </button>
             </Link>
           </div>
 
 
         </div>
-            
+
 
 
 
@@ -158,62 +159,71 @@ export const FacilityCard = ({fac_name,fac_img_url,fac_cont,fac_loact}) => {
   );
 };
 
-const EditDeletebuttons = () =>{
-  const [isOpen, setIsOpen] = useState(false)
-
-  function closeModal() {
-    setIsOpen(false)
+const UpdateDeletebuttons = ({ update_delete_title, Updateform }) => {
+  // Delete functions
+  const [isOpenDelete, setIsOpenDelete] = useState(false)
+  function closeModalDelete() {
+    setIsOpenDelete(false)
+  }
+  function openModalDelete() {
+    setIsOpenDelete(true)
+  }
+  // Edit functionS
+  const [isOpenEdit, setIsOpenEdit] = useState(false)
+  function closeModalEdit() {
+    setIsOpenEdit(false)
+  }
+  function openModalEdit() {
+    setIsOpenEdit(true)
   }
 
-  function openModal() {
-    setIsOpen(true)
-  }
-  return(
+  return (
     <>
-    <DeleteModal isOpen={isOpen} closeModal={closeModal}/>
+      <DeleteModal isOpen={isOpenDelete} closeModal={closeModalDelete} title={update_delete_title} />
+      <EditModal Updateform={Updateform} isOpen={isOpenEdit} closeModal={closeModalEdit} title={update_delete_title} />
 
-<div className="row my-3">
-          <div className="flex justify-between">
-            <div className="ms-3">
-              <button className="bg-purple-500 hover:bg-purple-600 flex p-2 rounded-full text-white relative" onClick={openModal}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-                  />
-                </svg>
-                <span className="text">Edit</span>
-              </button>
-            </div>
-            <div className="me-3">
-              <button className="bg-red-500 hover:bg-red-600 flex p-2 rounded-full text-white relative" onClick={openModal}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                  />
-                </svg>
-                <span className="text">Delete</span>
-              </button>
-            </div>
+      <div className="row my-3">
+        <div className="flex justify-between">
+          <div className="ms-3">
+            <button className="bg-purple-500 hover:bg-purple-600 flex p-2 rounded-2xl text-white relative" onClick={openModalEdit}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                />
+              </svg>
+              <span className="text">Update</span>
+            </button>
+          </div>
+          <div className="me-3">
+            <button className="bg-red-500 hover:bg-red-600 flex p-2 rounded-2xl text-white relative" onClick={openModalDelete}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                />
+              </svg>
+              <span className="text">Delete</span>
+            </button>
           </div>
         </div>
+      </div>
     </>
 
   );
