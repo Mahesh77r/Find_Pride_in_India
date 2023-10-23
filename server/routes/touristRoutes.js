d5591fa20eb04af37a24c3ecfc519bbf4cc1783b
Mahesh Rohane <shravanrohane19@gmail.com>
2023-10-23 21:38:40 +0530

    reset adn forgot password done

--- server/routes/touristRoutes.js
+++ server/routes/touristRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const {touristRegister, verifyOtp, touristLogin,sendOtp,getHello } = require("../controllers/touristController");
+const {touristRegister, verifyOtp, touristLogin,sendOtp,getHello, forgotpassword,resetpassword} = require("../controllers/touristController");
 
 // const auth = require("../middelweares/Authentication");
 
@@ -8,6 +8,8 @@ router.post("/register",touristRegister)
 router.post("/verifyotp",verifyOtp)
 router.post("/sendotp",sendOtp)
 router.post("/login",touristLogin)
+router.post("/forgotpassword",forgotpassword);
+router.post("/resetpassword/:token",resetpassword);
 router.get( '/hello' , getHello )
 
 
