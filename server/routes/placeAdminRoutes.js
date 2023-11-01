0995a05c8092022a94d552c379ade18010ccbd5d
Mahesh Rohane <shravanrohane19@gmail.com>
2023-11-01 21:50:02 +0530

    add and fetch product complete

--- server/routes/placeAdminRoutes.js
+++ server/routes/placeAdminRoutes.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
-const {placeAdminRegister, placeAdminlogin, wlcom } = require("../controllers/adminAuthController");
+const {placeAdminRegister, placeAdminlogin, wlcom,addProduct,getProducts } = require("../controllers/adminAuthController");
 
 const auth = require("../middelweares/Authentication");
 const upload = require("../middelweares/fileupload");
 
 router.post("/register",upload.single('file'),placeAdminRegister);
+router.post("/addproducts",upload.single('file'),addProduct);
+router.get("/getproducts",getProducts);
 router.post("/login",placeAdminlogin);
 router.get("/",auth,wlcom);
 
