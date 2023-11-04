66b0ff3ba0e66a96364797f856b886bfc8ee8cb9
Mahesh Rohane <shravanrohane19@gmail.com>
2023-11-04 20:59:44 +0530

    css added

--- client/src/pages/ResetPass.js
+++ client/src/pages/ResetPass.js
@@ -16,31 +16,49 @@ const ResetPassword = () => {
       });
 
       // Show a success message to the user
-      alert(
-        "Password reset successful! You can now log in with your new password."
-      );
+      alert("Password reset successful! You can now log in with your new password.");
       window.location.assign("/login");
       // Redirect the user to the login page or any other appropriate page
       // Example: props.history.push('/login');
     } catch (error) {
       console.error("Error resetting password:", error.response.data.error);
-      alert("Failed to reset password. Please try again later.",error.response.data.error);
+      alert("Failed to reset password. Please try again later.", error.response.data.error);
       window.location.assign("/login");
     }
   };
 
   return (
-    <div className="container items-center mt-5">
-      <form onSubmit={handleResetPassword}>
-        <label>New Password:</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <br />
-        <button type="submit">Reset Password</button>
-      </form>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="w-full max-w-xs">
+        <form
+          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+          onSubmit={handleResetPassword}
+        >
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              New Password:
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="New Password"
+            />
+          </div>
+          <div className="mb-6 text-center">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Reset Password
+            </button>
+          </div>
+        </form>
+        <p className="text-center text-gray-500 text-xs">
+          &copy; Finding Pride in INDIA {new Date().getFullYear()}
+        </p>
+      </div>
     </div>
   );
 };
