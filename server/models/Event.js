455ee17ea4f0d86706f9f2b413cb8b9e36485e6b
Mahesh Rohane <shravanrohane19@gmail.com>
2023-11-07 13:47:11 +0530

    change in Schma

--- server/models/Event.js
+++ server/models/Event.js
@@ -3,12 +3,11 @@ const mongoose = require('mongoose');
 const Event = new mongoose.Schema({
     
     event_name: { type: String ,require:true},
-    event_date: { type: String },
+    event_date: { type: Date },
     event_des: {type: String},
     city:{ type: String },
     state: { type: String },
     dest_name: { type: String },
-    filename: { type: String },
     path: { type: String },
 })
 
