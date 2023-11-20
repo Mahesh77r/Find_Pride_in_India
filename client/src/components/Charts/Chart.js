5fbb8cfdfa946f31f42f8bd87eb179a2e89be6dd
Ashwiniukhalkar <ashwiniukhalkar1@gmail.com>
2023-11-20 11:10:38 +0530

    ccomment added in chart.js

--- client/src/components/Charts/Chart.js
+++ client/src/components/Charts/Chart.js
@@ -4,7 +4,7 @@ import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
-
+// This is charts
 const GraphData = [
   {
     name:"John Doe",
