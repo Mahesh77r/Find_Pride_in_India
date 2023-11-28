0e4c09d7088913d6455dc47c50832b56a0de6232
Mahesh Rohane <shravanrohane19@gmail.com>
2023-11-28 10:08:37 +0530

    added mail and created getOrder with filter

--- server/controllers/ProductOrderForm.js
+++ server/controllers/ProductOrderForm.js
@@ -56,6 +56,7 @@ const createOrderForm = async (req, res) => {
 
     // Create a new order object
     const newOrder = new Order({
+      tourist_id:data.tourist_id,
       tourist_name: data.tourist_name,
       tourist_email: data.tourist_email,
       tourist_state: data.tourist_state,
@@ -68,6 +69,7 @@ const createOrderForm = async (req, res) => {
       product_name: data.product_name,
       product_quantity: data.product_quantity,
       product_price: data.product_price,
+      product_dest_id : data.product_dest_id
     });
 
     // Save the order to the database
@@ -91,4 +93,186 @@ const createOrderForm = async (req, res) => {
   }
 };
 
-module.exports = { createOrderForm };
+// getOrder controller
+const getOrder = async (req, res) => {
+  try {
+    const orders = await Order.find();
+
+    res.status(200).json({
+      success: true,
+      data: orders,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, error: `Error fetching orders: ${error}` });
+  }
+};
+
+// getOrderByTourist controller
+const getOrderByTourist = async (req, res) => {
+  try {
+    const tourist_id = req.params.tourist_id;
+
+    if (!tourist_id) {
+      return res.status(400).json({ success: false, error: 'Missing tourist_id parameter' });
+    }
+
+    const orders = await Order.find({ tourist_id: tourist_id });
+
+    res.status(200).json({
+      success: true,
+      data: orders,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, error: `Error fetching orders: ${error}` });
+  }
+};
+
+// getOrderByDest controller
+const getOrderByDest = async (req, res) => {
+  try {
+    const dest_id = req.params.dest_id;
+
+    if (!dest_id) {
+      return res.status(400).json({ success: false, error: 'Missing dest_id parameter' });
+    }
+
+    const orders = await Order.find({ product_dest_id: dest_id });
+
+    res.status(200).json({
+      success: true,
+      data: orders,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, error: `Error fetching orders: ${error}` });
+  }
+};
+
+const updateShippedStatus = async (req, res) => {
+  try {
+    const orderId = req.params.order_id;
+
+    if (!orderId) {
+      return res.status(400).json({ success: false, error: 'Missing order_id parameter' });
+    }
+
+    const updatedOrder = await Order.findByIdAndUpdate(
+      orderId,
+      { shipped_status: true },
+      { new: true }
+    );
+
+    if (!updatedOrder) {
+      return res.status(404).json({ success: false, error: 'Order not found' });
+    }
+
+    // Extract relevant data from the request
+    const { tourist_email, delivery_date } = req.body;
+
+    // Call sendShippedConfirmation
+    await sendShippedConfirmation(tourist_email, delivery_date);
+
+    res.status(200).json({
+      success: true,
+      // data: updatedOrder,
+      message: 'Shipped status updated and confirmation email sent successfully',
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, error: `Error updating shipped status: ${error}` });
+  }
+};
+
+// sendShippedConfirmation controller
+const sendShippedConfirmation = async (tourist_email, delivery_date) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to: tourist_email,
+      subject: 'Order Shipped Confirmation',
+      text: `
+        Hello from Finding Pride in India!
+        We're excited to let you know that your order has been shipped and is on its way to you.
+        It is scheduled to be delivered on ${delivery_date}.
+        Thank you for choosing our services. We hope you enjoy your products!
+        Happy exploring,
+        The Finding Pride in India Team`,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    console.log('Shipped confirmation email sent successfully');
+  } catch (error) {
+    console.error(`Error sending shipped confirmation email: ${error}`);
+  }
+};
+
+module.exports = { updateShippedStatus };
+
+
+// deleteOrderAndNotify controller
+const deleteOrderAndNotify = async (req, res) => {
+  try {
+    const orderId = req.params.order_id;
+    const { cancellation_reason } = req.body;
+
+    if (!orderId || !cancellation_reason) {
+      return res.status(400).json({ success: false, error: 'Missing required parameters' });
+    }
+
+    const deletedOrder = await Order.findByIdAndDelete(orderId);
+
+    if (!deletedOrder) {
+      return res.status(404).json({ success: false, error: 'Order not found' });
+    }
+
+    const transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to: deletedOrder.tourist_email,
+      subject: 'Order Cancellation Notification',
+      text: `
+        Hello from Finding Pride in India!
+        We regret to inform you that your order has been canceled due to the following reason:
+        ${cancellation_reason}
+        If you have any concerns or need further assistance, please contact our support team.
+        We apologize for any inconvenience caused.
+        Happy exploring,
+        The Finding Pride in India Team`,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    res.status(200).json({
+      success: true,
+      message: 'Order deleted and cancellation email sent successfully',
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      error: `Error deleting order and sending cancellation email: ${error}`,
+    });
+  }
+};
+
+
+
+module.exports = { createOrderForm, getOrder, getOrderByTourist, getOrderByDest,updateShippedStatus, sendShippedConfirmation , deleteOrderAndNotify};
